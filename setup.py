6091232f0d3ddb3011d00cc127e35dc67206c5d7
Unmani <68024805+Unmani199@users.noreply.github.com>
2024-10-22 18:31:30 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,6 @@
+import setuptools
+setuptools.setup(     
+     name=“resnet”,     
+     version="0.0.1",
+     python_requires=">=3.6",   
+     packages=[“modified_shannon_div”, “plots_stats_for_resilience”, “remove_ALL_selected_nodes”])
