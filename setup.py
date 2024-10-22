1262cd04c7f16b1ff6c64b16b8fcc1da91c19bb5
Unmani <68024805+Unmani199@users.noreply.github.com>
2024-10-22 19:14:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
-import setuptools
-setuptools.setup(     
-     name="sub-module",     
-     version="0.0.1",
-     python_requires=">=3.6",   
-     packages=["ResNet.py"])
+from setuptools import setup
+
+setup(
+    name='ResNet',
+    version='0.0.1',    
+    description='Python package to measure network resilience',
+    url='https://github.com/Unmani199/ResNet.git',
+    author='Unmani J',
+    author_email='unmani.jaygude@ucdconnect.ie',
+    license='None',
+    packages=['ResNet'],
+    python_requires=">=3.6",
+    install_requires=['pandas>=2.0.3','plotly>=5.9.0','networkx>=3.1'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: None :: None',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
