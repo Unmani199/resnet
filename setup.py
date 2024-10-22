06ad6826cedef347a99e623bfc8fdd37295dc233
Unmani <68024805+Unmani199@users.noreply.github.com>
2024-10-22 18:34:08 +0100

    Update setup.py
    Minor changes in syntax

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 setuptools.setup(     
-     name=“resnet”,     
+     name="resnet",     
      version="0.0.1",
      python_requires=">=3.6",   
-     packages=[“modified_shannon_div”, “plots_stats_for_resilience”, “remove_ALL_selected_nodes”])
+     packages=["modified_shannon_div", "plots_stats_for_resilience", "remove_ALL_selected_nodes"])
