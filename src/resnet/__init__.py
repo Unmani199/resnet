98346c574ba7a65c8825d6463a0a844786fea04c
Unmani <68024805+Unmani199@users.noreply.github.com>
2024-10-23 13:28:59 +0100

    Update __init__.py

--- src/resnet/__init__.py
+++ src/resnet/__init__.py
@@ -1,1 +1,5 @@
+# The root of the entire project
 
+# The version number of the code.  This will change each time a new release is made.  The most popular approach is to use Semantic Versioning,
+# described at https://semver.org/
+__version__ = "0.0.1"
