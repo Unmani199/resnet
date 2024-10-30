0aa5ad525a97b48e41669cb7f8cde236bfaa4be6
Unmani <68024805+Unmani199@users.noreply.github.com>
2024-10-30 16:35:29 +0000

    Update __init__.py

--- src/resnet/__init__.py
+++ src/resnet/__init__.py
@@ -1,2 +1,4 @@
 # src/resnet/__init__.py
+from .resnet_module import resnet  # Ensure this matches the actual class name in resnet_module.py
 
+__all__ = ["ResNet"]  # Optional: limits import * to include only ResNet
